d39dd6b7c3ff7e75e96e3a59f4b2de9c90488a24
Yash Sharma <yash4688@gmail.com>
2017-10-28 04:07:53 +0530

    change logic

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
+const readLastLines = require('read-last-lines');
 
 function driver(){
-
-    fs.readFile('output.txt', 'utf8', function(err, data) {  
-        if (err) throw err;
-        console.log(data);
-        if(data){
-            console.log("hello");
-        }
-        fs.writeFile('output.txt', '', function(){console.log('done')})
-    });
+    // fs.readFile('output.txt', 'utf8', function(err, data) {  
+    //     if (err) throw err;
+    //     console.log(data);
+    //     if(data) {
+    //         console.log("hello");
+    //     }
+    //     fs.writeFile('output.txt', '', function(){console.log('done')})
+    // });
+    readLastLines.read('output.txt', 1)
+    .then((lines) => console.log(lines));
 }
 
 console.log('before setInterval');
