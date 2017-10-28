ad333879c0847ac4e550095deb72833785e4f9bf
Yash Sharma <yash4688@gmail.com>
2017-10-28 05:37:08 +0530

    added speech

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const readLastLines = require('read-last-lines');
+const say = require('say')
 
 function driver(){
     // fs.readFile('output.txt', 'utf8', function(err, data) {  
@@ -11,7 +12,12 @@ function driver(){
     //     fs.writeFile('output.txt', '', function(){console.log('done')})
     // });
     readLastLines.read('output.txt', 1)
-    .then((lines) => console.log(lines));
+    .then((lines) => {
+        console.log(lines);
+        if(lines == 1){
+            say.speak("Object is in front of you");
+        }
+    });
 }
 
 console.log('before setInterval');
